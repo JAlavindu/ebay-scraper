09e2cde6b9f02f8b81020c512cd43a585a0d6c8f
JAlavindu <lavi156perera@gmail.com>
2026-03-20 23:14:52 +0530

    completed search results generation

--- src/main/java/com/ebaytest/BrowserCommands.java
+++ src/main/java/com/ebaytest/BrowserCommands.java
@@ -1,12 +1,8 @@
 package com.ebaytest;
 
-import java.text.Normalizer.Form;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 
 public class BrowserCommands {
 
@@ -15,15 +11,12 @@ public class BrowserCommands {
     TestBrowserCommands testBrowserCommands = new TestBrowserCommands();
 
     public void openBrowser(WebDriver driver) {
-        // WebDriverManager.chromedriver().setup();
-        // driver = new ChromeDriver();
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         
         driver.get("https://www.ebay.com/");
         String currentURL = driver.getCurrentUrl();
-        // testBrowserCommands(currentURL);
         testBrowserCommands.testBrowser(currentURL, driver);
         formInteraction.advancedNavigation(driver);
         
@@ -34,24 +27,5 @@ public class BrowserCommands {
             driver.quit();
         }
     }
-
-    // public void testBrowserCommands(String currentURL) {
-    //     System.out.println("=== Browser Commands Test ===");
-
-    //     Assert.assertTrue(
-    //             currentURL.contains("ebay.com"),
-    //             "Navigation failed! URL does not contain 'ebay.com'"
-    //         );
-        
-    //     Assert.assertFalse(
-    //             driver.getTitle().isEmpty(),
-    //             "Page title is empty — page may not have loaded!"
-    //         );
-
-    //     System.out.println("Navigation validated successfully!");
-    //     System.out.println("Page Title: " + driver.getTitle() + "character length: " + driver.getTitle().length());
-    //     System.out.println("Current URL: " + currentURL);
-    //     System.out.println("Page source length: " + driver.getPageSource().length());
-    // }
     
 }
--- src/main/java/com/ebaytest/FormInteraction.java
+++ src/main/java/com/ebaytest/FormInteraction.java
@@ -1,9 +1,11 @@
 package com.ebaytest;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Options;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -24,32 +26,54 @@ public class FormInteraction {
         keywordInput.sendKeys("laptop");
         System.out.println("Keyword entered: " + keywordInput.getAttribute("value"));
 
-        WebElement categorydropdown = driver.findElement(By.xpath("//div[@class='field adv-field___sacat']//select[contains(@id,'s0-1-20-4')]"));
-        Select categorySelect = new Select(categorydropdown);
-        categorySelect.selectByIndex(1);
-        //categorySelect.selectByVisibleText("Exact words, exact order");
+        WebElement keywordCategorydropdown = driver.findElement(By.xpath("//div[@class='field adv-field___sacat']//select[contains(@id,'s0-1-20-4')]"));
+        Select keywordCategorySelect = new Select(keywordCategorydropdown);
+        keywordCategorySelect.selectByIndex(1);
+        WebElement selectedOption = keywordCategorySelect.getFirstSelectedOption();
+        System.out.println("selected option: " + selectedOption.getText());
+        //keywordCategorySelect.selectByVisibleText("Exact words, exact order");
+        //keywordCategorySelect.selectByValue("2");
 
+        List<WebElement> optionsList = keywordCategorySelect.getOptions();
+
+        for(WebElement option : optionsList){
+            String optionText = option.getText();
+            String optionValue = option.getAttribute("value");
+    
+            System.out.println("Text: '" + optionText + "' | Value: '" + optionValue + "'");
+        }
+
+        //checkboxes
         WebElement searchCheckbox1 = driver.findElement(By.id("s0-1-20-5[1]-[0]-LH_TitleDesc"));
         searchCheckbox1.click();
         Assert.assertTrue(searchCheckbox1.isSelected(), "Search checkbox 1 was not selected!");
 
-        WebElement searchCheckbox2 = driver.findElement(By.className("checkbox__control"));
+        WebElement searchCheckbox2 = driver.findElement(By.name("LH_Complete"));
         searchCheckbox2.click();
         Assert.assertTrue(searchCheckbox2.isSelected(), "Search checkbox 2 was not selected!");
 
+        //min and max price
         WebElement minPricElement = driver.findElement(By.cssSelector("input[name='_udlo']"));
         minPricElement.sendKeys("100");
         String actualMinPrice = minPricElement.getAttribute("value");
         Assert.assertEquals(actualMinPrice, "100", "Minimum price was not set correctly!");
         System.out.println("Minimum price validated successfully: " + actualMinPrice);
 
-        WebElement maxPricElement = driver.findElement(By.cssSelector("input[id='s0-1-20-5[2]-@range-comp[]-@range-textbox[]_1-textbox']"));
+        WebElement maxPricElement = driver.findElement(By.name("_udhi"));
         maxPricElement.sendKeys("500");
         String actualMaxPrice = maxPricElement.getAttribute("value");
         Assert.assertEquals(actualMaxPrice, "500", "Maximum price was not set correctly!");
         System.out.println("Maximum price validated successfully: " + actualMaxPrice);
 
+        //radio buttons
+        WebElement buyingFormatRadio = driver.findElement(By.id("s0-1-20-6[3]-[0]-LH_BO"));
+        buyingFormatRadio.click();
+
+        WebElement conditionRadio = driver.findElement(By.id("s0-1-20-6[4]-[0]-LH_ItemCondition"));
+        conditionRadio.click();
+
         WebElement searchButton = driver.findElement(By.xpath("//div[@class='adv-form__actions']//button[@type='submit'][normalize-space()='Search']"));
+        System.out.println("button name:" + searchButton.getText());
         searchButton.click();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
