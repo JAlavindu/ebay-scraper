15921b389abc2503e8947d545b65e921946ffda0
JAlavindu <lavi156perera@gmail.com>
2026-03-22 18:06:14 +0530

    Test Navigation - Navigate to eBay and verify page title

--- src/main/java/com/ebaytest/question2/TestNGBasicsTest.java
+++ src/main/java/com/ebaytest/question2/TestNGBasicsTest.java
@@ -1,10 +1,15 @@
 package com.ebaytest.question2;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestNGBasicsTest {
     WebDriver driver;
@@ -12,6 +17,19 @@ public class TestNGBasicsTest {
     @BeforeClass
     public void setUp() {
         System.out.println("@BeforeClass: Setting up test environment");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testNavigation(){
+        System.out.println("@Test: Navigating to eBay and verifying title");
+
+        driver.get("https://www.ebay.com/");
+        String pageTitle = driver.getTitle();
+        System.out.println("Page title: " + pageTitle);
+        Assert.assertTrue(pageTitle.contains("eBay"), "Page title does not contain 'eBay'!");
     }
 
     @BeforeTest
@@ -27,6 +45,9 @@ public class TestNGBasicsTest {
     @AfterClass
     public void endTest() {
         System.out.println("@AfterClass: Cleaning up test environment");
+        if(driver != null) {
+            driver.quit();
+        }
     }
 
 }
